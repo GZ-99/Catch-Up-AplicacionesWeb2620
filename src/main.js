81d65fb6f40ca373e1d7a4652c882b5aa357379b
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-16 09:52:30 -0500

    chore: configure primevue icons, flex and material theme

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Button, SelectButton} from "primevue";
+
+const primeUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 
 createApp(App)
     .use(i18n)
+    .use(PrimeVue, {
+        ripple: true,
+        theme: { preset: Material },
+        license: primeUiLicenseKey
+    })
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
     .mount('#app')
