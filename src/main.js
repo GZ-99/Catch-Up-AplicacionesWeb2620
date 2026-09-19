5c8f72acb685cd82f44e3542c6cdd45b44a96804
Alberto Sanchez <albertosanchez05@gmail.com>
2026-09-19 07:25:48 -0500

    feat(main): add use primevue components

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import PrimeVue from 'primevue/config';
 import Material from '@primeuix/themes/material';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import {Button, SelectButton} from "primevue";
+import {Avatar, Button, Card, Drawer, Menu, Menubar, Popover, SelectButton, Toolbar, Tooltip} from "primevue";
 
 const primeUiLicenseKey = import.meta.env.VITE_PRIME_UI_LICENSE_KEY;
 
@@ -19,4 +19,12 @@ createApp(App)
     })
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-card', Card)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-popover', Popover)
+    .directive('tooltip', Tooltip)
     .mount('#app')
